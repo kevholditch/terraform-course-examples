89276f01a951b437c16980b1829ea75f156361db
Kevin Holditch <kevholditch@hotmail.com>
2018-02-21 16:48:28 +0000

    new example

---
+++ example03/main.tf
@@ -0,0 +1,30 @@
+
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_s3_bucket" "my_bucket" {
+  bucket = "kevholditch-myfirst-bucket"
+}
+
+resource "aws_iam_policy" "my_bucket_policy" {
+  name = "my-bucket-policy"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:ListBucket"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${data.aws_s3_bucket.my_bucket.arn}"
+      ]
+    }
+  ]
+}
+POLICY
+}
