a9da431cfa26f7645f70319f0ef170296a791f6f
Kevin Holditch <kevholditch@hotmail.com>
2018-02-25 19:25:57 +0000

    example 6

---
+++ example06/main.tf
@@ -0,0 +1,32 @@
+
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource aws_s3_bucket "my_bucket" {
+  bucket = "kevholditch-bucket"
+}
+
+
+data "template_file" "bucket_policy" {
+  template = "${file("policy.json")}"
+
+  vars {
+    bucket_arn = "${aws_s3_bucket.my_bucket.arn}"
+  }
+}
+
+resource "aws_iam_user" "kevin" {
+  name = "Kevin-Holditch"
+}
+
+resource "aws_iam_user_policy" "my_bucket_policy" {
+  name   = "my-bucket-policy"
+  user   = "${aws_iam_user.kevin.name}"
+  policy = "${data.template_file.bucket_policy.rendered}"
+}
+
+output "policy" {
+  value = "${aws_iam_user_policy.my_bucket_policy.policy}"
+}
